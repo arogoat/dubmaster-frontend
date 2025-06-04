d509a8812c5321c51ff7f20bc87856923475580c
Arkadiusz Kuska <zenekorgin@gmail.com>
2025-06-04 02:54:16 +0200

    lol

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx'; // ← musi być .jsx
+import App from './App.jsx'; // ← to jest KLUCZOWE
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(

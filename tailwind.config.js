c865133c78794843ecdb787a4ef80b97dc1891bd
Arkadiusz Kuska <zenekorgin@gmail.com>
2025-06-04 02:38:26 +0200

    leim

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './index.html',
+    './src/**/*.{js,jsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
